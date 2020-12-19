3986b50f215e41450123a6eadda8326041cf0162
andreeastnc <st.andreea10@gmail.com>
2020-12-19 14:31:29 +0200

    First commit

---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject groundTile;
+    Vector3 nextSpawnPoint;
+
+    public void SpawnTile(bool spawnItems)
+    {
+        GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
+        nextSpawnPoint = temp.transform.GetChild(1).transform.position;
+
+        if (spawnItems)
+        {
+            temp.GetComponent<GroundTIle>().SpawnObstacle();
+            temp.GetComponent<GroundTIle>().SpawnCoins();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            if (i < 2)
+            {
+                SpawnTile(false);
+            } else
+            {
+                SpawnTile(true);
+            }
+        }
+    }
+
+}
